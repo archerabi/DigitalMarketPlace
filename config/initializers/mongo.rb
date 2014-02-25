d04366d7b3a88af2d6710a7d3a48df3aa78e4266
Abhijith Reddy <abhijithrc@ondeck.com>
2014-02-24 21:18:27 -0500

    Mongo config fix

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,5 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "DigitalMarketplace-#{Rails.env}"
+#MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+#MongoMapper.database = "DigitalMarketplace-#{Rails.env}"
 
 if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
